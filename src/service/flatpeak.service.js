a77b418a1c271430cd5b841fec98ffb2571fba84
Alert1437 <a.a.cherkaev@gmail.com>
2023-03-24 22:34:32 +0200

    Remove hardcoded credentials

--- src/service/flatpeak.service.js
+++ src/service/flatpeak.service.js
@@ -2,8 +2,8 @@ import { FlatpeakService } from "@flat-peak/javascript-sdk";
 global.Buffer = global.Buffer || require("buffer").Buffer;
 
 const service = new FlatpeakService(
-  "https://stg-api.flatpeak.energy",
-  "pk_test_ZVqvEgrZBIe4ZxVcnWCOzwoTphiv9IGF",
+  "https://api.flatpeak.energy",
+  "",
   (message) => {
     console.log("[SERVICE]: " + message);
   }
